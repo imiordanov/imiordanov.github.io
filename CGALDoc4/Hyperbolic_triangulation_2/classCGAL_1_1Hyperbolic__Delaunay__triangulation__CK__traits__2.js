0144bab85284ff37d0ac2f4e04a5525bd977d557
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-08-24 15:40:26 +0200

    new version of CGAL documentation

---
+++ CGALDoc4/Hyperbolic_triangulation_2/classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.js
@@ -0,0 +1,9 @@
+var classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2 =
+[
+    [ "Circular_arc_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#a308f27728fd03a4cdf6977982f9bcf4a", null ],
+    [ "Hyperbolic_segment_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#a35af8c1aca67848670a73841e2e09802", null ],
+    [ "Line_arc_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#af9f264a6749912b459e0da44495627b8", null ],
+    [ "Voronoi_point_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#a14422eebd59595ab4ac9bb55b2a9af92", null ],
+    [ "Hyperbolic_Delaunay_triangulation_CK_traits_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#a33a86ccce727c0d7a0a4870c20a39ea0", null ],
+    [ "Hyperbolic_Delaunay_triangulation_CK_traits_2", "classCGAL_1_1Hyperbolic__Delaunay__triangulation__CK__traits__2.html#af76ef470cc515386c3b4db3c6ca0ce82", null ]
+];
---
+++ CGALDoc4/Hyperbolic_triangulation_2/group__PkgHyperbolicTriangulation2VertexFaceClasses.js
@@ -0,0 +1,15 @@
+var group__PkgHyperbolicTriangulation2VertexFaceClasses =
+[
+    [ "Hyperbolic_triangulation_face_base_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__2.html", [
+      [ "Hyperbolic_triangulation_face_base_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__2.html#ad0127578d7ccb6bae9500677d65d7384", null ],
+      [ "Hyperbolic_triangulation_face_base_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__2.html#a06d3412899c57143599d36b40b4c7d5a", null ],
+      [ "Hyperbolic_triangulation_face_base_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__2.html#a74135a832945857beddb939fd1365baf", null ]
+    ] ],
+    [ "Hyperbolic_triangulation_face_base_with_info_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html", [
+      [ "Hyperbolic_triangulation_face_base_with_info_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html#a68f7d23b2bfe609bd90250076f75e7b7", null ],
+      [ "Hyperbolic_triangulation_face_base_with_info_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html#aa5e2c9f0e7c623347dcc8c6c51daf768", null ],
+      [ "Hyperbolic_triangulation_face_base_with_info_2", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html#a56a801d2f08dbe029f30abba73b1f7ff", null ],
+      [ "info", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html#ab59bce40630601c40079ec2b7eb787c4", null ],
+      [ "info", "classCGAL_1_1Hyperbolic__triangulation__face__base__with__info__2.html#a630ff0d775c2005c563464b17b0ab228", null ]
+    ] ]
+];
