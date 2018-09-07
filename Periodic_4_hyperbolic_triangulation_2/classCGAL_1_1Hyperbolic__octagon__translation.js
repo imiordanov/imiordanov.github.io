90e582e96aefc6da11c7768794ec3dd1bd2184f6
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-09-07 11:28:33 +0200

    updated documentation for Periodic package

---
+++ Periodic_4_hyperbolic_triangulation_2/classCGAL_1_1Hyperbolic__octagon__translation.js
@@ -0,0 +1,4 @@
+var classCGAL_1_1Hyperbolic__octagon__translation =
+[
+    [ "to_string", "classCGAL_1_1Hyperbolic__octagon__translation.html#a6c984be3a7f25d821f8cf0bb78d5cd95", null ]
+];
---
+++ Periodic_4_hyperbolic_triangulation_2/group__PkgPeriodic4HyperbolicTriangulation2Concepts.js
@@ -0,0 +1,62 @@
+var group__PkgPeriodic4HyperbolicTriangulation2Concepts =
+[
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html", [
+      [ "Coefficients", "classHyperbolicOctagonTranslation.html#adafdf01e695ff4cf28ca766b705097b7", null ],
+      [ "Complex", "classHyperbolicOctagonTranslation.html#a5d3d15934224bf479143877c6cab7983", null ],
+      [ "NT", "classHyperbolicOctagonTranslation.html#aa94e0099959151a052a2d3ba293d263e", null ],
+      [ "Word", "classHyperbolicOctagonTranslation.html#aa7bd9d932fe09299e0dd46668d40e1fa", null ],
+      [ "Word_letter", "classHyperbolicOctagonTranslation.html#acb964ee0616864d9d9ce4f0517aff3d7", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#aae9c4574d46b7242da817a397d407757", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a32dcafff8c2cf0442db79eb4e375206b", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#acc2b373b1e014f3f0ad775f861be1e5d", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#aff1da172d08412373939ae6067462c26", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a5aea4bfa8a437f2f0231961186bc4376", null ],
+      [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a2f0eb8296ee6f1b45f4195d1921ee336", null ],
+      [ "alpha", "classHyperbolicOctagonTranslation.html#ab66a4189b38dbf47b56fa0b242e8a0a9", null ],
+      [ "beta", "classHyperbolicOctagonTranslation.html#a89765c6da41b2c9953e5c37e1617b955", null ],
+      [ "get_generator_coefficients", "classHyperbolicOctagonTranslation.html#a15555cd8b9c76aafbadb967afccf6468", null ],
+      [ "get_generators", "classHyperbolicOctagonTranslation.html#a59140a4a4080d5e70a9904cf049c8918", null ],
+      [ "inverse", "classHyperbolicOctagonTranslation.html#a66845ed4a3cef127193b2250699a1022", null ],
+      [ "is_identity", "classHyperbolicOctagonTranslation.html#afd9b65e38715d9dde5f6fcb8e58eaf79", null ],
+      [ "operator!=", "classHyperbolicOctagonTranslation.html#af25cefc28e0bc16cb3ea70127fb160cd", null ],
+      [ "operator*", "classHyperbolicOctagonTranslation.html#ae2d1b157decf45aa2c563137d6718fc2", null ],
+      [ "operator-", "classHyperbolicOctagonTranslation.html#a5e8971e33ed248518f227c1fa271d6c9", null ],
+      [ "operator<", "classHyperbolicOctagonTranslation.html#adb0891a4a0bcf39d9cce77a1ad83dced", null ],
+      [ "operator=", "classHyperbolicOctagonTranslation.html#a077ea6cfe9f4308005ab82a89e258b50", null ],
+      [ "operator==", "classHyperbolicOctagonTranslation.html#a991250beb2a05a85a17fc1d33d405e69", null ]
+    ] ],
+    [ "Periodic_4HyperbolicDelaunayTriangulationTraits_2", "classPeriodic__4HyperbolicDelaunayTriangulationTraits__2.html", [
+      [ "Compute_approximate_hyperbolic_diameter", "classPeriodic__4HyperbolicDelaunayTriangulationTraits__2.html#a5eaeae3225aac22b0fbbf300be1b2a78", null ],
+      [ "compute_approximate_hyperbolic_diameter_object", "classPeriodic__4HyperbolicDelaunayTriangulationTraits__2.html#a8e6bd081ec6afc187bd47ae8c11bbee5", null ]
+    ] ],
+    [ "Periodic_4HyperbolicTriangulationFaceBase_2", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html", [
+      [ "Hyperbolic_translation", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#ad2146295ab7185db16d816306fcb5c27", null ],
+      [ "Periodic_4HyperbolicTriangulationFaceBase_2", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#afb53cda22107cc8f3b5680a1625e0b48", null ],
+      [ "Periodic_4HyperbolicTriangulationFaceBase_2", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#a1406a41ea4e925cf0eb73232ef29e961", null ],
+      [ "Periodic_4HyperbolicTriangulationFaceBase_2", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#a5b6760e84fd902965434e255963d5e1c", null ],
+      [ "reorient", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#a8515c535c2968282628964395e127f65", null ],
+      [ "set_translation", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#a2ecf18c207d7f6f3605f2b1a599d7484", null ],
+      [ "translation", "classPeriodic__4HyperbolicTriangulationFaceBase__2.html#adf7cfc8af58a6120572e0ad2c150ae4d", null ]
+    ] ],
+    [ "Periodic_4HyperbolicTriangulationTraits_2", "classPeriodic__4HyperbolicTriangulationTraits__2.html", [
+      [ "Circle_2", "classPeriodic__4HyperbolicTriangulationTraits__2.html#a3bb0b5d810585ce69e3ad4f955b7604a", null ],
+      [ "Construct_point_2", "classPeriodic__4HyperbolicTriangulationTraits__2.html#a73641028697b00cf15290633469bcde7", null ],
+      [ "Hyperbolic_translation", "classPeriodic__4HyperbolicTriangulationTraits__2.html#a5bdc1ec688844a5bdf3aa63c9160193d", null ],
+      [ "Side_of_original_octagon", "classPeriodic__4HyperbolicTriangulationTraits__2.html#ae74163db9eaef8de0b45c6a1702f4dd9", null ],
+      [ "construct_point_2_object", "classPeriodic__4HyperbolicTriangulationTraits__2.html#a00b0cfa2baab8113d57afa1d561a7bdd", null ],
+      [ "side_of_original_octagon_object", "classPeriodic__4HyperbolicTriangulationTraits__2.html#a11204b8e150c2ba687d6b7597abd55bc", null ]
+    ] ],
+    [ "Periodic_4HyperbolicTriangulationVertexBase_2", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html", [
+      [ "Face_handle", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a8b83d3733b38e133e71b6779d616a4f6", null ],
+      [ "Hyperbolic_translation", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a4cb496c9f5d5b3738d82766be53f4ee9", null ],
+      [ "Point", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a9739a9d7aa8f1e460a6a7b264edf911e", null ],
+      [ "Periodic_4HyperbolicTriangulationVertexBase_2", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a043e851c8e523a4dc8132fc646d59a35", null ],
+      [ "Periodic_4HyperbolicTriangulationVertexBase_2", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a6af874452af8a91a8bd37a8b02efdc19", null ],
+      [ "Periodic_4HyperbolicTriangulationVertexBase_2", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#ab900071e5e85381465d57af0b4c446ef", null ],
+      [ "Periodic_4HyperbolicTriangulationVertexBase_2", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#ace5b00efd46487ef95c20bf76b8ff20b", null ],
+      [ "clear_translation", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a9097c7a4a46b87c81f339545838dc454", null ],
+      [ "get_translation_flag", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#adb5edc44492839dce527f1b48066182e", null ],
+      [ "set_translation", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#ae64a960bbd763dc0ccd7fad4ca949b90", null ],
+      [ "translation", "classPeriodic__4HyperbolicTriangulationVertexBase__2.html#a7debbedeee95a80fa6faa67037157d32", null ]
+    ] ]
+];
