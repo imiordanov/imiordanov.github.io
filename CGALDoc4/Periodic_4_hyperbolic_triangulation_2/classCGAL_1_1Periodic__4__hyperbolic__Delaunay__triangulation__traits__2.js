401d6d6c623db692b52edeb6dc75ff955597f540
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-09-07 11:29:59 +0200

    updated documentation for Periodic package

---
+++ CGALDoc4/Periodic_4_hyperbolic_triangulation_2/classCGAL_1_1Periodic__4__hyperbolic__Delaunay__triangulation__traits__2.js
@@ -0,0 +1,5 @@
+var classCGAL_1_1Periodic__4__hyperbolic__Delaunay__triangulation__traits__2 =
+[
+    [ "Hyperbolic_translation", "classCGAL_1_1Periodic__4__hyperbolic__Delaunay__triangulation__traits__2.html#ae8c58f8d7ebcb2a9d3fac7882ee1f675", null ],
+    [ "NT", "classCGAL_1_1Periodic__4__hyperbolic__Delaunay__triangulation__traits__2.html#a9494d9616b1851d0149d756bd39996a0", null ]
+];
