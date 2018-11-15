05c9987be761bb15bdb9ab9f35cbd399971c56ab
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-11-15 13:44:11 +0100

    modifications to documentation

---
+++ CGALDoc6/Periodic_4_hyperbolic_triangulation_2/classHyperbolicOctagonTranslation.js
@@ -0,0 +1,26 @@
+var classHyperbolicOctagonTranslation =
+[
+    [ "Coefficients", "classHyperbolicOctagonTranslation.html#adafdf01e695ff4cf28ca766b705097b7", null ],
+    [ "Complex", "classHyperbolicOctagonTranslation.html#a5d3d15934224bf479143877c6cab7983", null ],
+    [ "NT", "classHyperbolicOctagonTranslation.html#aa94e0099959151a052a2d3ba293d263e", null ],
+    [ "Word", "classHyperbolicOctagonTranslation.html#aa7bd9d932fe09299e0dd46668d40e1fa", null ],
+    [ "Word_letter", "classHyperbolicOctagonTranslation.html#acb964ee0616864d9d9ce4f0517aff3d7", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#aae9c4574d46b7242da817a397d407757", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a32dcafff8c2cf0442db79eb4e375206b", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#acc2b373b1e014f3f0ad775f861be1e5d", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#aff1da172d08412373939ae6067462c26", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a5aea4bfa8a437f2f0231961186bc4376", null ],
+    [ "HyperbolicOctagonTranslation", "classHyperbolicOctagonTranslation.html#a2f0eb8296ee6f1b45f4195d1921ee336", null ],
+    [ "alpha", "classHyperbolicOctagonTranslation.html#ab66a4189b38dbf47b56fa0b242e8a0a9", null ],
+    [ "beta", "classHyperbolicOctagonTranslation.html#a89765c6da41b2c9953e5c37e1617b955", null ],
+    [ "get_generator_coefficients", "classHyperbolicOctagonTranslation.html#a15555cd8b9c76aafbadb967afccf6468", null ],
+    [ "get_generators", "classHyperbolicOctagonTranslation.html#a59140a4a4080d5e70a9904cf049c8918", null ],
+    [ "inverse", "classHyperbolicOctagonTranslation.html#a66845ed4a3cef127193b2250699a1022", null ],
+    [ "is_identity", "classHyperbolicOctagonTranslation.html#afd9b65e38715d9dde5f6fcb8e58eaf79", null ],
+    [ "operator!=", "classHyperbolicOctagonTranslation.html#af25cefc28e0bc16cb3ea70127fb160cd", null ],
+    [ "operator*", "classHyperbolicOctagonTranslation.html#ae2d1b157decf45aa2c563137d6718fc2", null ],
+    [ "operator-", "classHyperbolicOctagonTranslation.html#a5e8971e33ed248518f227c1fa271d6c9", null ],
+    [ "operator<", "classHyperbolicOctagonTranslation.html#adb0891a4a0bcf39d9cce77a1ad83dced", null ],
+    [ "operator=", "classHyperbolicOctagonTranslation.html#a077ea6cfe9f4308005ab82a89e258b50", null ],
+    [ "operator==", "classHyperbolicOctagonTranslation.html#a991250beb2a05a85a17fc1d33d405e69", null ]
+];
