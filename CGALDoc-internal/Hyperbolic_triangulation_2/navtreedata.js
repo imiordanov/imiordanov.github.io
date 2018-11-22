dc4986da4c335ab4f937f1c2af741e60af7d02b7
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-11-22 13:47:12 +0100

    internal version of documentation

---
+++ CGALDoc-internal/Hyperbolic_triangulation_2/navtreedata.js
@@ -0,0 +1,29 @@
+var NAVTREE =
+[
+  [ "CGAL 4.14 - 2D Hyperbolic Delaunay Triangulations", "index.html", [
+    [ "User Manual", "index.html", [
+      [ "The Poincaré Disk Model of the Hyperbolic Plane", "index.html#HT2_Poincare_model", null ],
+      [ "Euclidean and Hyperbolic Delaunay Triangulations", "index.html#HT2_Euclidean_and_hyperbolic_Delaunay_triangulations", null ],
+      [ "Software Design", "index.html#HT2_Software_design", null ],
+      [ "Examples", "index.html#Examples", null ],
+      [ "Design and Implementation History", "index.html#HT2_Design", null ]
+    ] ],
+    [ "Reference Manual", "modules.html", "modules" ],
+    [ "Refinement Relationships", "refines.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Is Model Relationships", "models.html", null ],
+    [ "Has Model Relationships", "hasModels.html", null ],
+    [ "MODIFICATIONS", "Modification.html", null ],
+    [ "Bibliography", "citelist.html", null ],
+    [ "Class and Concept List", "annotated.html", "annotated" ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"Hyperbolic_triangulation_2_2ht2_example_8cpp-example.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
