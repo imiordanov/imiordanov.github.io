92bc55cab69e9213ddc57ac7a5060d8b0f5008d5
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-12-05 07:45:17 +0100

    new version of user manual

--- CGALDoc7/Hyperbolic_triangulation_2/navtreedata.js
+++ CGALDoc7/Hyperbolic_triangulation_2/navtreedata.js
@@ -6,6 +6,7 @@ var NAVTREE =
       [ "Euclidean and Hyperbolic Delaunay Triangulations", "index.html#HT2_Euclidean_and_hyperbolic_Delaunay_triangulations", null ],
       [ "Software Design", "index.html#HT2_Software_design", null ],
       [ "Examples", "index.html#Examples", null ],
+      [ "Performance", "index.html#HT2_Performance", null ],
       [ "Design and Implementation History", "index.html#HT2_Design", null ]
     ] ],
     [ "Reference Manual", "modules.html", "modules" ],
